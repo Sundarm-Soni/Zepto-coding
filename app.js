f28d1e5dffb57e8d0ac1e69260c5d898f0b63eb9
Sundarm <sundarm.soni097@gmail.com>
2024-04-22 20:35:02 +0530

    feature/Expense management app

---
+++ app.js
@@ -0,0 +1,105 @@
+    const finalSummary = document.querySelector('.summary-container');
+    const expenseContainer = document.querySelector('.expense-container');
+
+    const data = [
+        {
+            name: "Rohit",
+            expense: 0,
+        },
+        {
+            name: "Akshay",
+            expense: 0,
+        },
+        {
+            name: "Sundarm",
+            expense: 0,
+        }
+    ];
+
+    const showInputFields = () => {
+        const memberInputs = data.map((val) => {
+            return `
+            <label class="expense-label">${val.name}</label>
+            <input type="number" placeholder="Enter the expense" class="expense-input" data-name=${val.name}/>
+            `;
+        });
+
+
+        memberInputs.forEach(member => {
+            const divElement = document.createElement('div')
+            divElement.innerHTML = member;
+            divElement.classList.add('expense');
+            expenseContainer.append(divElement);
+        });
+
+        //900 600 300 -600 -400 -200
+    }
+
+    const distributeExpense = (enteredAmounts) => {
+        const noOfPeople = data.length;
+        let maxAmount = Number.MIN_VALUE;
+        let finalDistribution = 0;
+
+        enteredAmounts.forEach((amount) => {
+            console.log(amount)
+            maxAmount = Math.max(maxAmount, amount.expense);
+        });
+
+        
+        enteredAmounts.forEach((amount) => {
+            if(amount.expense < maxAmount) {
+                finalDistribution -= amount.expense;
+            }
+        });
+        console.log(finalDistribution)
+        expenseToBeDistributed = finalDistribution / noOfPeople;
+        // 400, 300, 100 
+        // 100/3
+
+        const finalSummary = data.map((people) => {
+            return {
+                ...people,
+                expense: expenseToBeDistributed
+            }       
+        });
+        return finalSummary;
+    }
+
+    const showSummary = (members) => {
+        let fragment = document.createDocumentFragment();
+        let ul = document.createElement('ul');
+        ul.classList.add('summary');
+        members.forEach((member) => {
+            const li = document.createElement('li');
+            const memberName = document.createElement('span');
+            const expenseForEach = document.createElement('span');
+            memberName.innerText = member.name;
+            expenseForEach.innerHTML = member.expense;
+            li.appendChild(memberName);
+            li.appendChild(expenseForEach);
+            fragment.appendChild(li);
+        });
+        ul.append(fragment);
+        finalSummary.appendChild(ul);
+    }
+
+
+    window.addEventListener('DOMContentLoaded', () => {
+        showInputFields();
+
+        
+        const expenseBtn = document.querySelector('.expense-btn');
+        const expenseInput = document.querySelectorAll('.expense-input');
+
+        expenseBtn.addEventListener('click', (e) => {
+            e.stopPropagation();
+            const enteredValues = Array.from(expenseInput).map((input, index) => {
+                    return {
+                        name: input.dataset.name,
+                        expense: input.value
+                    }
+            })
+            console.log(enteredValues)
+            showSummary(distributeExpense(enteredValues));
+        });
+    });
---
+++ readme.md
@@ -0,0 +1,7 @@
+Bill splitting app (like splitwise)
+- create a group
+- add a expense and split among people
+- add a expense factor and support custom split as well
+- generate a expense summary for group
+- Settle the expense in group
+- optimize the settlement in such a way that final amount distribution is not heavy on one person
